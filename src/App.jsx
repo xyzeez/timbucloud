fbb602cea3daca3f3a76945469f1af689265ff57
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-07-08 09:25:47 +0100

    ✨ feat(app): added app routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 // Components
 import AppLayout from './components/AppLayout,';
 
+// Pages
+import Home from './pages/Home';
+
 const App = () => {
   return (
     <BrowserRouter>
       <AppLayout>
         <Routes>
-          <Route index element={<Navigate replace />} />
+          <Route index element={<Navigate replace to="home" />} />
+          <Route path="home" element={<Home />} />
         </Routes>
       </AppLayout>
     </BrowserRouter>
