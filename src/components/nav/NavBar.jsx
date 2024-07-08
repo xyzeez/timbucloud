6a187de86e73351022b43da0385d5e30df3a9f22
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-07-08 09:18:32 +0100

    🎨 ui(app): added nav component

---
+++ src/components/nav/NavBar.jsx
@@ -0,0 +1,62 @@
+import Logo from '../Logo';
+
+const NavBar = () => {
+  return (
+    <header className="flex flex-col items-center gap-y-2 px-4 py-3">
+      <div className="grid grid-cols-2 sm:grid-cols-[1fr_auto] w-full gap-x-2 font-inter font-medium text-lg text-[#000000]/50">
+        <label className="flex flex-row items-center gap-x-2 w-full overflow-hidden border border-blue-light rounded-md p-2">
+          <img src="/public/assets/search.png" alt="" className="size-6" />
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Search"
+            className="w-full"
+          />
+          <span className="sr-only">Search for an item</span>
+        </label>
+        <div className="flex flex-row items-center justify-between gap-x-2 w-full max-w-[250px] overflow-hidden border border-blue-light rounded-md p-2">
+          <p>Dashboard</p>
+          <img
+            src="/public/assets/chevron-down.png"
+            alt=""
+            className="size-6"
+          />
+        </div>
+      </div>
+      <div className="flex flex-row items-center lg:grid lg:grid-cols-[auto_1fr_auto] gap-x-6 w-full">
+        <Logo />
+        <button className="ml-auto sm:order-3 sm:m-0 lg:flex lg:flex-row-reverse lg:items-center lg:gap-4 lg:p-3 text-white text-center font-inter font-bold text-lg lg:bg-blue rounded">
+          <img src="/public/assets/cart.png" className="lg:hidden" alt="" />
+          <img
+            src="/public/assets/cart-dark.png"
+            className="hidden lg:block"
+            alt=""
+          />
+          <span className="hidden lg:inline ml-2 uppercase">Shopping Cart</span>
+        </button>
+        <nav className="sm:ml-auto lg:w-full max-w-[640px]">
+          <button className="sm:hidden">
+            <img src="/public/assets/nav-open.png" alt="" />
+          </button>
+          <ul className="hidden sm:flex sm:flex-row sm:gap-6 justify-between w-full font-inter font-medium text-lg uppercase">
+            <li>
+              <a href="#">Products</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            <li>
+              <a href="#">Affiliate</a>
+            </li>
+            <li>
+              <a href="#">Special</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
---
+++ src/components/nav/NavForm.jsx
@@ -0,0 +1,9 @@
+const NavForm = () => {
+  return (
+    <form>
+      <input type="search" name="" id="" />
+    </form>
+  );
+};
+
+export default NavForm;
