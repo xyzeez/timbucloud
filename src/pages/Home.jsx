8a642bb97d9ee749e6ff1bca1962f8035e35cd06
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-07-08 09:26:48 +0100

    🎨 ui(app): added home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,19 @@
+import Product from '../components/product/Product';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main className="flex flex-col gap-y-6 lg:gap-y-10 px-4 pb-6">
+      <picture>
+        <source srcSet="/assets/home/hero-web.png" media="(min-width: 640px)" />
+        <img
+          src="/assets/home/hero-mobile.png"
+          alt=""
+          className="w-full rounded"
+        />
+      </picture>
+      <Product />
+    </main>
+  );
 };
 
 export default Home;
